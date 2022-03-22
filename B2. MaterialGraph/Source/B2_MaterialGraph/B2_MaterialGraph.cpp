f5246998c8ee3e4d2b90528cc880b8fe5730788f
Dobri Y <dyordan2@gmail.com>
2022-03-21 23:00:16 -0400

    Material Graph Examples

---
+++ B2. MaterialGraph/Source/B2_MaterialGraph/B2_MaterialGraph.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "B2_MaterialGraph.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, B2_MaterialGraph, "B2_MaterialGraph" );
---
+++ B2. MaterialGraph/Source/B2_MaterialGraph/B2_MaterialGraphGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "B2_MaterialGraphGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class B2_MATERIALGRAPH_API AB2_MaterialGraphGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
